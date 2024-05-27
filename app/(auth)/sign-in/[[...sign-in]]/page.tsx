39eb496f80655e680f0f94335093cd9e3c8ae783
qolvap <pgrebennikow@yahoo.pl>
2024-05-27 18:43:44 +0200

    clerk auth set

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,12 @@
+import { SignIn, SignUp } from '@clerk/nextjs'
+import React from 'react'
+
+const SignInPage = () => {
+  return (
+    <div>
+      <SignIn/>
+    </div>
+  )
+}
+
+export default SignInPage
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 
 const Home= () => {
   return (
     <div>
-      Home
+      <p>Home</p>
+      <SignedOut>          
+        <SignInButton />        
+        </SignedOut>        
+        <SignedIn>          
+        <UserButton afterSignOutUrl='/' />       
+          </SignedIn>
     </div>
   )
 }
