ab3980e7849743fc032421636860ab4144d39cd6
qolvap <pgrebennikow@yahoo.pl>
2024-06-12 15:57:39 +0200

    add img form input field

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -14,55 +14,50 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { defaultValues } from "@/constants"
+import { CustomField } from "./CustomField"
 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
+export const formSchema = z.object({
+  title: z.string(),
+  aspectRatio: z.string().optional(),
+  color: z.string().optional(),
+  prompt: z.string().optional(),
+  publicId: z.string(),
 })
 
-export function ProfileForm() {
-  // 1. Define your form.
+const TransformationForm = ({action, data = null}: TransformationFormProps) => {
+  const initialValues = data && action === 'Update' ? {
+    title: data.title,
+    aspectRatio: data.aspectRatio,
+    color: data.color,
+    prompt: data.prompt,
+    publicId: data.publicId,
+  }: defaultValues
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: {
-      username: "",
-    },
+    defaultValues: initialValues,
   })
 
-  // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log(values)
   }
-}
 
-
-const TransformationForm = () => {
-    return (
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <FormField
-              control={form.control}
-              name="username"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Username</FormLabel>
-                  <FormControl>
-                    <Input placeholder="shadcn" {...field} />
-                  </FormControl>
-                  <FormDescription>
-                    This is your public display name.
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button type="submit">Submit</Button>
-          </form>
-        </Form>
-      )
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <CustomField 
+            control={form.control}
+            name="publicId"
+            className="flex size-full flex-col"
+            render={({ field }) => <Input {...field}
+            className="input-field" />}
+          />
+      </form>
+    </Form>
+  )
 }
 
 export default TransformationForm
